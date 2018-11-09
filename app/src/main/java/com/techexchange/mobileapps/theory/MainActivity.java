e5f7ecdd310c2a179b00f98d0f409b4470833f7f
Biswash Adhikari <biswash@biswash-macbookpro.roam.corp.google.com>
2018-11-08 16:36:39 -0800

    second commit

--- app/src/main/java/com/techexchange/mobileapps/theory/MainActivity.java
+++ app/src/main/java/com/techexchange/mobileapps/theory/MainActivity.java
@@ -7,11 +7,13 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int current= 2;
-    String outputText="User System  Win/Lose \n";
-//    final TextView showOutput = (TextView) findViewById(R.id.textId);
+    int choiceLeft=2;
+    String outputText="\n\n User     System     Win/Lose \n\n\n";
+    private TextView showOutput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,90 +22,120 @@ public class MainActivity extends AppCompatActivity {
         final ImageButton btnClickTwo=(ImageButton) findViewById(R.id.ImageButton02);
         final ImageButton btnClickThree=(ImageButton) findViewById(R.id.ImageButton03);
         final ImageButton btnClickFour = (ImageButton) findViewById(R.id.ImageButton04);
-//        final TextView showOutput = (TextView) findViewById(R.id.textId);
+        showOutput = findViewById(R.id.textId);
+        showOutput.setText(outputText);
         btnClickOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnClickOne.setImageResource(R.drawable.onegreen);
+                if(current==1 && choiceLeft>0) {
+                    btnClickOne.setImageResource(R.drawable.onegreen);
+                    Toast.makeText(MainActivity.this, "Congratulations!", Toast.LENGTH_SHORT).show();
+                }else{
+                    choiceLeft=choiceLeft-1;
+                }
             }
         });
         btnClickTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnClickTwo.setImageDrawable(getDrawable(R.drawable.twogreen));
+                if(current==2 && choiceLeft>0) {
+                    btnClickTwo.setImageResource(R.drawable.twogreen);
+                    Toast.makeText(MainActivity.this, "Congratulations!", Toast.LENGTH_SHORT).show();
+                }else{
+                    choiceLeft=choiceLeft-1;
+                }
             }
         });
         btnClickThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnClickThree.setImageDrawable(getDrawable(R.drawable.threegreen));
+                if(current==3 && choiceLeft>0) {
+                    btnClickThree.setImageResource(R.drawable.threegreen);
+                    Toast.makeText(MainActivity.this, "Congratulations!", Toast.LENGTH_SHORT).show();
+                }else{
+                    choiceLeft=choiceLeft-1;
+                }
             }
         });
 
         btnClickFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnClickFour.setImageDrawable(getDrawable(R.drawable.fourgreen));
+                if(current==4 && choiceLeft>0) {
+                    btnClickFour.setImageResource(R.drawable.fourgreen);
+                    Toast.makeText(MainActivity.this, "Congratulations!", Toast.LENGTH_SHORT).show();
+                }else{
+                    choiceLeft=choiceLeft-1;
+                }
             }
         });
     }
 
-    private void playGameR(View view){
+    public void playGameR(View view){
         if (current==2){
             current=1;
             outputText+="Rock   Paper   Lose \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else if(current==1){
             current=3;
             outputText+="Rock   Rock   Draw \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else if(current==3){
             current=2;
             outputText+="Rock   Paper   Lose \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else{
             current=3;
             outputText+="Rock   Scissors   Win \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }
     }
-    private void playGameP(View view){
+    public void playGameP(View view){
         if (current==2){
             current=1;
             outputText+="Paper   Paper   Draw \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else if(current==1){
             current=3;
             outputText+="Paper   Rock   Win \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else if(current==3){
             current=4;
             outputText+="Paper   Paper   Draw \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else{
             current=1;
             outputText+="Paper   Scissors   Lose \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }
 
     }
-    private void playGameS(View view){
+    public void playGameS(View view){
         if (current==2){
             current=4;
             outputText+="Scissors   Paper   Win \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else if(current==1){
             current=3;
             outputText+="Scissors   Rock   Lose \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else if(current==3){
             current=1;
             outputText+="Scissors   Paper   Win \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }else{
             current=1;
             outputText+="Scissors   Scissors   Draw \n";
-//            showOutput.setText(outputText);
+            showOutput.setText(outputText);
         }
     }
+    public void reset(View view) {
+        current=0;
+        outputText="";
+        showOutput.setText(outputText);
+        Toast.makeText(MainActivity.this, "Game has been reset!", Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
